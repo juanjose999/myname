479509703a2348aa9ed03270b125be18d353d6e7
juan jose s.o <juanjose000012@gmail.com>
2024-03-04 15:00:11 -0500

    the api response with my name

--- src/main/java/com/myname/api/controller/MyNameController.java
+++ src/main/java/com/myname/api/controller/MyNameController.java
@@ -1,5 +1,7 @@
 package com.myname.api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyNameController {
 
     @GetMapping
-    public String printlMyName(){
-        return "juan jose sierra ortega";
+    public ResponseEntity<?> printMyName(){
+        return new ResponseEntity<>("! Juan Jose Sierra Ortega !", HttpStatus.OK) ;
     }
 }
